63578461a4319c2e9df1618d2f05a73ef48bda25
Preetham Reddy Gollapalli <136400069+PreethamGollapalli@users.noreply.github.com>
2024-09-09 16:17:17 -0700

    Update analytics_query.sql

--- analytics_query.sql
+++ analytics_query.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `data-with-darshil.uber_data_engineering_yt.tbl_analytics` AS (
+CREATE OR REPLACE TABLE `tbl_analytics` AS (
 SELECT 
 f.trip_id,
 f.VendorID,
@@ -21,12 +21,12 @@ f.improvement_surcharge,
 f.total_amount
 FROM 
 
-`data-with-darshil.uber_data_engineering_yt.fact_table` f
-JOIN `data-with-darshil.uber_data_engineering_yt.datetime_dim` d  ON f.datetime_id=d.datetime_id
-JOIN `data-with-darshil.uber_data_engineering_yt.passenger_count_dim` p  ON p.passenger_count_id=f.passenger_count_id  
-JOIN `data-with-darshil.uber_data_engineering_yt.trip_distance_dim` t  ON t.trip_distance_id=f.trip_distance_id  
-JOIN `data-with-darshil.uber_data_engineering_yt.rate_code_dim` r ON r.rate_code_id=f.rate_code_id  
-JOIN `data-with-darshil.uber_data_engineering_yt.pickup_location_dim` pick ON pick.pickup_location_id=f.pickup_location_id
-JOIN `data-with-darshil.uber_data_engineering_yt.dropoff_location_dim` drop ON drop.dropoff_location_id=f.dropoff_location_id
-JOIN `data-with-darshil.uber_data_engineering_yt.payment_type_dim` pay ON pay.payment_type_id=f.payment_type_id)
+`fact_table` f
+JOIN `datetime_dim` d  ON f.datetime_id=d.datetime_id
+JOIN `passenger_count_dim` p  ON p.passenger_count_id=f.passenger_count_id  
+JOIN `trip_distance_dim` t  ON t.trip_distance_id=f.trip_distance_id  
+JOIN `rate_code_dim` r ON r.rate_code_id=f.rate_code_id  
+JOIN `pickup_location_dim` pick ON pick.pickup_location_id=f.pickup_location_id
+JOIN `dropoff_location_dim` drop ON drop.dropoff_location_id=f.dropoff_location_id
+JOIN `payment_type_dim` pay ON pay.payment_type_id=f.payment_type_id)
 ;
